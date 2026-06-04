1ec58445665e9388159fd3faff71cb654a83dde0
Miguel Junior <mjinfo01@gmail.com>
2026-06-03 23:31:20 -0300

    Corrige base do GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // O nome abaixo DEVE ser exatamente o nome que aparece na URL do seu repositório
-  base: "/APP-ORCAMENTO/", 
+  base: '/app-orcamento/',
 })
